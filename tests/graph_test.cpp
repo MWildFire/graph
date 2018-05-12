ea9ab1a6ee6d67ef14785677273de3a0170e163d
MWildFire <mvstrike17@gmail.com>
2018-05-13 01:54:42 +0300

    UPD2

--- tests/graph_test.cpp
+++ tests/graph_test.cpp
@@ -5,7 +5,7 @@
 TEST_CASE ("creating, printing graph, adding edge")
 {
     graph_t graph(4);
-    grapg.addEdge(1,2);
+    graph.addEdge(1,2);
     graph.addEdge(2, 3);
     graph.addEdge(3,4);
     std::string output
@@ -16,14 +16,14 @@ TEST_CASE ("creating, printing graph, adding edge")
         "0 0 1 0\n"
     };
     std::ostringstream stream;
-    graph.print(stream);
+    graph.printMatrix(stream);
     REQUIRE(stream.str()==output);
 }
 
 TEST_CASE ("removing edges")
 {
     graph_t graph(6);
-    grapg.addEdge(1,2);
+    graph.addEdge(1,2);
     graph.addEdge(2, 3);
     graph.addEdge(3,4);
     graph.removeEdge(3,4);
@@ -35,21 +35,21 @@ TEST_CASE ("removing edges")
         "0 0 0 0\n"
     };
     std::ostringstream stream;
-    graph.print(stream);
+    graph.printMatrix(stream);
     REQUIRE(stream.str()==output);
 }
     
     
-}
+
 
 TEST_CASE ("testing DFS")
 {
     graph_t graph(6);
-    grapg.addEdge(1,2);
+    graph.addEdge(1,2);
     graph.addEdge(2, 3);
     graph.addEdge(3,4);
     graph.DFS();
-    VertexState *state = new VertexState[vertexCount];
-    for (int i = 0; i < vertexCount; i++)
-        REQUIRE (state[i] = Black);
+    graph_t::VertexState *state = new graph_t::VertexState[graph_t::vertexCount];
+    for (int i = 0; i < graph_t::vertexCount; i++)
+        REQUIRE (state[i] = graph_t::Black);
 }
