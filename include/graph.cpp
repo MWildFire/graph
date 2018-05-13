1c8a18f3c9d13a2f1a5bbfc949a264f7da1359b9
MWildFire <mvstrike17@gmail.com>
2018-05-13 16:03:40 +0300

    Update graph.cpp

--- include/graph.cpp
+++ include/graph.cpp
@@ -86,7 +86,6 @@ public:
         for (int i = 0; i < vertexCount; i++)
             state[i] = White;
         runDFS(0, state);
-        delete [] state;
     }
     
     void runDFS(int u, VertexState state[])
