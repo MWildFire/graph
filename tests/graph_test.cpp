d648cfa7fb4c53185c6ba8a88d03a12cef82c16e
MWildFire <mvstrike17@gmail.com>
2018-05-13 15:53:08 +0300

    Update graph_test.cpp

--- tests/graph_test.cpp
+++ tests/graph_test.cpp
@@ -51,10 +51,10 @@ TEST_CASE ("removing edges")
 TEST_CASE ("testing DFS")
 {
     graph_t graph(6);
-    graph.addEdge(4,5);
-    graph.addEdge(1,4);
-    graph.addEdge(4,2);
-    graph.addEdge(5,3);
+    graph.addEdge(0,1);
+    graph.addEdge(1,2);
+    graph.addEdge(2,3);
+    graph.addEdge(3,4);
     graph.DFS();
     graph_t::VertexState state[6];
     for (int i = 0; i < 6; i++)
