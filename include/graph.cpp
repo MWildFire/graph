b6b41604e046a9fc4ab41809ec2b59c605514ec5
MWildFire <mvstrike17@gmail.com>
2018-05-13 17:50:48 +0300

    Update graph.cpp

--- include/graph.cpp
+++ include/graph.cpp
@@ -102,7 +102,7 @@ public:
         VertexState *state = new VertexState[vertexCount];
         for (int i = 0; i < vertexCount; i++)
         {
-            if (state[i] != White)
+            if (state[i] != 3)
             {
                 return false;
             } 
