ed7d04939367d6eec0979822244275dcb27e23ff
MWildFire <mvstrike17@gmail.com>
2018-05-13 13:27:37 +0300

    .

--- tests/graph_test.cpp
+++ tests/graph_test.cpp
@@ -4,15 +4,18 @@
 
 TEST_CASE ("creating, printing graph, adding edge")
 {
-    graph_t graph(4);
-    graph.addEdge(2,3);
-    graph.addEdge(3,4);
+    graph_t graph(5);
+    graph.addEdge(1,4);
+    graph.addEdge(4,2);
+    graph.addEdge(4,5);
+    graph.addEdge(5,3);
     std::string output
     {
-        "0 0 0 0\n"
-        "0 0 1 0\n"
-        "0 1 0 1\n"
-        "0 0 1 0\n"
+        "0 0 0 1 0\n"
+        "0 0 0 1 1\n"
+        "0 0 0 0 1\n"
+        "1 1 0 0 1\n"
+        "0 1 1 1 0\n"
     };
     std::ostringstream stream;
     graph.printMatrix(stream);
@@ -21,19 +24,19 @@ TEST_CASE ("creating, printing graph, adding edge")
 
 TEST_CASE ("removing edges")
 {
-    graph_t graph(6);
-    graph.addEdge(2,3);
-    graph.addEdge(3,4);
+    graph_t graph(5);
+    graph.addEdge(1,4);
+    graph.addEdge(4,2);
     graph.addEdge(4,5);
-    graph.removeEdge(3,4);
+    graph.addEdge(5,3);
+    graph.removeEdge(5,3);
     std::string output
     {
-        "0 0 0 0 0 0\n"
-        "0 0 1 0 0 0\n"
-        "0 1 0 0 0 0\n"
-        "0 0 0 0 1 0\n"
-        "0 0 0 1 0 0\n"
-        "0 0 0 0 0 0\n"
+        "0 0 0 1 0\n"
+        "0 0 0 1 1\n"
+        "0 0 0 0 0\n"
+        "1 1 0 0 1\n"
+        "0 1 0 1 0\n"
     };
     std::ostringstream stream;
     graph.printMatrix(stream);
@@ -45,16 +48,14 @@ TEST_CASE ("removing edges")
 
 TEST_CASE ("testing DFS")
 {
-    graph_t graph(6);
-    graph.addEdge(1,2);
-    graph.addEdge(2,3);
-    graph.addEdge(3,4);
+    graph_t graph(5);
+    graph.addEdge(1,4);
+    graph.addEdge(4,2);
+    graph.addEdge(4,5);
+    graph.addEdge(5,3);
     graph.DFS();
     graph_t::VertexState *state = new graph_t::VertexState[6];
     for (int i = 0; i < 6; i++)
     REQUIRE (state[i] == graph_t::Black);
 }
-
-    
-    
 
